f0755c0158cfa1606e4ed93151f7ac7bff869f80
Bastien Fournier <bastien.fournier06@gmail.com>
2024-03-19 10:53:12 +0100

    "ajout prénom en couleur"

---
+++ index.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+
+
+
+const a = "Salma";
+const b = "Léo-Paul"
+const c = "Kana"
+const d = "Bastien"
+
+console.log(chalk.green(a));
+console.log(chalk.cyan(b));
+console.log(chalk.magentaBright(c));
+console.log(chalk.bgRed(d));
+
